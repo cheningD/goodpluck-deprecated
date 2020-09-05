ff0c6ec58ebc01ccf736c6a2ef28ceff180c2923
Jamesbarford <jamesbarfordevans@gmail.com>
2020-09-05 23:27:23 +0100

    mobile menu done

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,7 +1,9 @@
 import * as React from "react"
+import "./Hamburger.css"
+import { listToClass } from "../util"
 
 export const Hamburger = React.memo(props => (
-  <button onClick={props.onOpen} className="hamburger__wrapper">
+  <button onClick={props.onOpen} className={listToClass(["hamburger", props.isOpen && "hamburger__open"])}>
     <span className="hamburger-line hamburger--line-top" />
     <span className="hamburger-line hamburger--line-middle" />
     <span className="hamburger-line hamburger--line-bottom" />
