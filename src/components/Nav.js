c672cbaccdcf65a58b9810acfda98e562604e26c
Jamesbarford <jamesbarfordevans@gmail.com>
2020-09-08 22:57:52 +0100

    feature: search bar ui component mobile & desktop (#37)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import "./Nav.css"
 import { useShoppingCart } from "use-shopping-cart"
+import Select from "react-select"
 
 import { listToClass } from "../util"
 
@@ -9,12 +10,16 @@ import { Hamburger } from "./Hamburger"
 import { Menu } from "./Menu"
 
 const Nav = () => {
-  const { cartCount } = useShoppingCart()
   const [isMobileNavOpen, setMobileNavIsOpen] = React.useState()
+  const [showSearchBar, setShowSearchBar] = React.useState()
 
   const toggleMobileNavOpen = React.useCallback(() => {
     setMobileNavIsOpen(!isMobileNavOpen)
   }, [isMobileNavOpen])
+
+  const toggleSearchBar = React.useCallback(() => {
+    setShowSearchBar(!showSearchBar)
+  }, [showSearchBar])
 
   return (
     <div
@@ -26,60 +31,54 @@ const Nav = () => {
           <Link to="/" className="brand">
             GOODPLUCK
           </Link>
-          <button className="nav-search--btn">
-            <span className="search-icon"></span>
-          </button>
+          <SearchBar toggleSearchBar={toggleSearchBar} />
         </div>
-
         <div className="nav-items">
-          <button className="nav-search--btn">
-            <span className="search-text">Search</span>
-          </button>
-
-          <div className="header-links--list--wrapper">
-            <div
-              className={listToClass([
-                "header-links--list",
-                isMobileNavOpen && "header-links--list__open",
-              ])}
-            >
-              <div className="header-link--wrapper">
-                <Link
-                  to="/mylists"
-                  className={listToClass([
-                    "header-link",
-                    linkIsActive("/mylists") && "current-link",
-                  ])}
-                >
-                  My Lists
-                </Link>
-              </div>
-              <div className="header-link--wrapper">
-                <Link
-                  to="/signin"
-                  className={listToClass([
-                    "header-link",
-                    linkIsActive("/signin"),
-                  ])}
-                >
-                  Sign in
-                </Link>
-              </div>
-              <div className="header-link--wrapper">
-                <Link
-                  to="/cart"
-                  className={listToClass([
-                    "header-link",
-                    linkIsActive("/cart"),
-                  ])}
-                >
-                  Cart: {`${cartCount}`}
-                </Link>
-              </div>
-            </div>
-          </div>
+          {(!showSearchBar || isMobileNavOpen) && (
+            <Links isMobileNavOpen={isMobileNavOpen} />
+          )}
           <Menu />
         </div>
+      </div>
+    </div>
+  )
+}
+
+const Links = ({ isMobileNavOpen }) => {
+  const { cartCount } = useShoppingCart()
+  return (
+    <div
+      className={listToClass([
+        "header-links--list",
+        isMobileNavOpen && "header-links--list__open",
+      ])}
+    >
+      <div className="header-link--wrapper">
+        <Link
+          to="/mylists"
+          className={listToClass([
+            "header-link",
+            linkIsActive("/mylists") && "current-link",
+          ])}
+        >
+          My Lists
+        </Link>
+      </div>
+      <div className="header-link--wrapper">
+        <Link
+          to="/signin"
+          className={listToClass(["header-link", linkIsActive("/signin")])}
+        >
+          Sign in
+        </Link>
+      </div>
+      <div className="header-link--wrapper">
+        <Link
+          to="/cart"
+          className={listToClass(["header-link", linkIsActive("/cart")])}
+        >
+          Cart: {`${cartCount}`}
+        </Link>
       </div>
     </div>
   )
@@ -90,3 +89,59 @@ function linkIsActive(path) {
 }
 
 export default Nav
+
+const SearchBar = React.memo(props => {
+  const [isFocused, setFocused] = React.useState(false)
+
+  const shouldCloseMenuOnScroll = e => {
+    if (e.target instanceof HTMLElement) {
+      return !e.target.className.includes("nav-search")
+    }
+    return false
+  }
+
+  const toggleFocused = React.useCallback(() => {
+    setFocused(!isFocused)
+    props.toggleSearchBar()
+  }, [isFocused])
+
+  return (
+    <Select
+      backspaceRemovesValue={true}
+      menuShouldScrollIntoView={false}
+      openMenuOnFocus={false}
+      openMenuOnClick={false}
+      noOptionsMessage={() => "No items match search"}
+      className={listToClass([
+        "nav-search--wrapper",
+        isFocused && "nav-search--wrapper__focused",
+      ])}
+      classNamePrefix="nav-search"
+      onFocus={toggleFocused}
+      onBlur={toggleFocused}
+      menuPortalTarget={document.body}
+      closeMenuOnScroll={shouldCloseMenuOnScroll}
+      options={[{ label: "1", value: 1 }]}
+      placeholder={
+        <>
+          <span style={{ marginRight: "5px" }} className="fas fa-solid">
+            
+          </span>
+          {!isFocused && (
+            <span className="nav-search--placeholder-text">Search</span>
+          )}
+        </>
+      }
+      components={{
+        IndicatorSeparator: null,
+        DropdownIndicator: null,
+      }}
+      styles={{
+        menuPortal: style => ({
+          ...style,
+          zIndex: 2,
+        }),
+      }}
+    />
+  )
+})
