d9e57f607d6d1fd831a1acc320afaf4d8a707305
Chening Duker <cdduker@gmail.com>
2020-09-09 19:34:50 -0400

    (Fixes #13) Privacy & Terms Links fixed, other links hidden

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,26 +10,34 @@ export default function Footer() {
         <div>© WhatsEatLike, Inc.</div>
       </div>
       <div className="footer-links">
-        <a href="/" className="footer-link">
+        {/* <a href="https://www.facebook.com/goodpluck" className="footer-link">
           Facebook
-        </a>
-        <a href="/" className="footer-link">
+        </a> */}
+        {/* <a href="https://twitter.com/goodpluckgrows" className="footer-link">
           Twitter
-        </a>
-        <a href="/" className="footer-link">
+        </a> */}
+        <a
+          href="https://www.instagram.com/goodpluckgrows/"
+          className="footer-link"
+        >
           Instagram
         </a>
-        <Link to="/" className="footer-link">
+        {/* <Link to="/" className="footer-link">
           Sell On Goodpluck
-        </Link>
-        <Link to="/" className="footer-link">
+        </Link> */}
+        <Link to="/privacy" className="footer-link">
           Privacy
         </Link>
-        <Link to="/" className="footer-link">
+
+        <Link to="/terms" className="footer-link">
           Terms &amp; Conditions
+        </Link>
+        <Link to="/returns" className="footer-link">
+          Returns, Refunds & Credits
         </Link>
       </div>
       <iframe
+        title="newsletter-signup"
         className="footer-newsletter-signup"
         src="https://cdn.forms-content.sg-form.com/2628788e-f24d-11ea-8e09-06b692f79d11"
       />
---
+++ src/pages/terms.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Nav from "../components/Nav"
+import Footer from "../components/footer"
+
+export default function Privacy() {
+  return (
+    <>
+      <Nav />
+
+      <div
+        name="termly-embed"
+        data-id="b2d69771-b7ea-40c3-a44a-9618b080cb45"
+        data-type="iframe"
+      ></div>
+      <script type="text/javascript">
+        {(function (d, s, id) {
+          var js,
+            tjs = d.getElementsByTagName(s)[0]
+          if (d.getElementById(id)) return
+          js = d.createElement(s)
+          js.id = id
+          js.src = "https://app.termly.io/embed-policy.min.js"
+          tjs.parentNode.insertBefore(js, tjs)
+        })(document, "script", "termly-jssdk")}
+      </script>
+      <Footer />
+    </>
+  )
+}
