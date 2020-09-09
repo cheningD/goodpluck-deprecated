d3b31e0e2380378d90c9d8b386d2cf4e1be9f707
Chening Duker <cdduker@gmail.com>
2020-09-09 15:31:56 -0400

    Connected to sendgrid

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import "./index.css"
 import Nav from "../components/Nav"
+import Footer from "../components/footer"
 
 export const query = graphql`
   query DepartmentQuery {
@@ -174,47 +175,7 @@ export default function Home({ data }) {
           </div>
         </div>
       </div>
-      <div className="footer">
-        <div className="footer-column">
-          <h4 className="h41">Goodpluck</h4>
-          <div>© WhatsEatLike, Inc.</div>
-        </div>
-        <div className="footer-column">
-          <a href="/" className="a2">
-            Facebook
-          </a>
-          <a href="/" className="a2">
-            Twitter
-          </a>
-          <a href="/" className="a2">
-            Instagram
-          </a>
-        </div>
-        <div className="footer-column">
-          <div>Our newsletter is also super fresh:</div>
-          <div className="newsletter">
-            <form className="form1">
-              <input
-                type="text"
-                className="newsletter-input"
-                placeholder="Email Address"
-              />
-              <button className="newsletter-button">Ok</button>
-            </form>
-          </div>
-        </div>
-        <div className="footer-column">
-          <Link to="/" className="a2">
-            Sell On Goodpluck
-          </Link>
-          <Link to="/" className="a2">
-            Privacy
-          </Link>
-          <Link to="/" className="a2">
-            Terms &amp; Conditions
-          </Link>
-        </div>
-      </div>
+      <Footer />
     </div>
   )
 }
