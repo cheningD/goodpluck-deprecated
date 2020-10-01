98bfbb1fdda71ce52fcf24f9544fd85c2a982778
Chening Duker <cdduker@gmail.com>
2020-09-30 21:07:01 -0400

    added chat to wrapper component, A little dirty but it should do

--- wrap-with-cart-provider.js
+++ wrap-with-cart-provider.js
@@ -1,21 +1,25 @@
 import React from "react"
 import { loadStripe } from "@stripe/stripe-js"
 import { CartProvider } from "use-shopping-cart"
+import MissiveChatBox from "./src/components/MissiveChatBox"
 
 const stripePromise = loadStripe(process.env.GATSBY_STRIPE_API_PUBLIC_KEY)
 
 export default ({ element }) => {
   return (
-    <CartProvider
-      mode="client-only"
-      stripe={stripePromise}
-      successUrl="https://goodpluck.com/checkoutsuccess?session_id={CHECKOUT_SESSION_ID}"
-      cancelUrl="https://goodpluck.com/cart?cancel=1"
-      currency="USD"
-      allowedCountries={["US"]}
-      billingAddressCollection={true}
-    >
-      {element}
-    </CartProvider>
+    <>
+      <MissiveChatBox />
+      <CartProvider
+        mode="client-only"
+        stripe={stripePromise}
+        successUrl="https://goodpluck.com/checkoutsuccess?session_id={CHECKOUT_SESSION_ID}"
+        cancelUrl="https://goodpluck.com/cart?cancel=1"
+        currency="USD"
+        allowedCountries={["US"]}
+        billingAddressCollection={true}
+      >
+        {element}
+      </CartProvider>
+    </>
   )
 }
