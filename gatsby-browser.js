81f3f93380fc81bf738c62bbc06b7b1d2d78375b
Chening Duker <cdduker@gmail.com>
2020-10-06 16:56:30 -0400

    improve lighthouse score6

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,5 +8,7 @@ import wrapWithCartProvider from "./wrap-with-cart-provider"
 export const wrapRootElement = wrapWithCartProvider
 
 if (!process.env.GATSBY_STRIPE_SHIPPING_LINE_ITEM_PRICE_ID) {
-  throw "Missing required env variable GATSBY_STRIPE_SHIPPING_LINE_ITEM_PRICE_ID"
+  throw new Error(
+    "Missing required env variable: GATSBY_STRIPE_SHIPPING_LINE_ITEM_PRICE_ID"
+  )
 }
