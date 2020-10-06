4bd71bebd114ffdc76c8083fe8d28b1b5fe920ec
Chening Duker <cdduker@gmail.com>
2020-10-06 04:30:41 -0400

    audit fixes5

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,24 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GoodPluck",
+        short_name: "GoodPluck",
+        start_url: "/",
+        background_color: "#788474",
+        theme_color: "#788474",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
