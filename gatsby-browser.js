3d2480d9ca2155397e0b41382db0de9594500603
Chening Duker <cdduker@gmail.com>
2020-10-06 13:10:52 -0400

    update workflow

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,12 @@
-import wrapWithCartProvider from "./wrap-with-cart-provider"
-
 import "./src/styles/normalize.css"
 import "./src/styles/fonts.css"
 import "./src/styles/goodpluck.css"
 import "./src/styles/react-select-search.css"
 
+import wrapWithCartProvider from "./wrap-with-cart-provider"
+
 export const wrapRootElement = wrapWithCartProvider
+
+if (!process.env.GATSBY_STRIPE_SHIPPING_LINE_ITEM_PRICE_ID) {
+  throw "Missing required env variable GATSBY_STRIPE_SHIPPING_LINE_ITEM_PRICE_ID "
+}
