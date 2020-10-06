cadf8dc2e533d9f57ffcb20033da3d7353e41aca
Chening Duker <cdduker@gmail.com>
2020-10-06 12:28:12 -0400

    fix delivery date, cart shipping logic

--- src/components/MissiveChatBox.js
+++ src/components/MissiveChatBox.js
@@ -2,9 +2,15 @@ import React from "react"
 import ScriptTag from "react-script-tag"
 
 const MissiveChatBox = () => {
+  if (typeof window === `undefined`) {
+    return ""
+  }
+
   window.MissiveChatConfig = {
     id: "1ea1215d-b61c-4638-b7b1-65acdb00bd1c",
   }
+  console.log("Settuing up chat box")
+
   return (
     <ScriptTag
       src={
