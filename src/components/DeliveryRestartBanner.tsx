8a252aaed07a815d42e8194ec4ea0ed01cb3f419
Chening Duker <cdduker@gmail.com>
2025-03-18 14:22:13 -0400

    typo

--- src/components/DeliveryRestartBanner.tsx
+++ src/components/DeliveryRestartBanner.tsx
@@ -18,7 +18,7 @@ const DeliveryRestartBanner = () => {
       <Flex maxW="7xl" mx="auto" px={{ base: '4', md: '8' }} alignItems="center" justifyContent="center">
         <Icon as={FiTruck} boxSize={{ base: 5, md: 6 }} mr={3} />
         <Text fontWeight="medium" fontSize={{ base: 'sm', md: 'md' }} textAlign="center">
-          Springh is here, we missed you! Fresh deliveries return April 3rd.
+          Spring is here, we missed you! Fresh produce deliveries return April 3rd.
         </Text>
         <CloseButton
           position="absolute"
